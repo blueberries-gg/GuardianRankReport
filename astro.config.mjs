38fbb03d455ee0460430ba5f297ee5909bf27d3c
nznaza <jgnz@live.com.mx>
2024-06-10 17:43:47 -0600

    feat: use base url for deploy in non-root pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nznaza.github.io',
+  base: 'GuardianRankReport',
+  trailingSlash: 'never',
   integrations: [solidJs({devtools: true})],
   vite: {
     plugins: [basicSsl()],
