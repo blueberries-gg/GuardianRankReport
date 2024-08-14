9d9e4dc29c34e5977c7098e6028bebf58e97a144
nznaza <jgnz@live.com.mx>
2024-08-14 02:12:49 -0600

    fix: add missing hash for RoN

--- src/utils/destinyActivities/raids.ts
+++ src/utils/destinyActivities/raids.ts
@@ -219,7 +219,7 @@ export const RaidsBaseDefinitions: { [key in keyof typeof DestinyRaid]: IActivit
 
 	RootOfNightmares: {
 		Modes: {
-			Normal: [2381413764],
+			Normal: [2381413764, 1191701339/* Missing??? Thannks Tom*/],
 			Master: [2918919505],
 		},
 		Type: ActivityType.Raid,
